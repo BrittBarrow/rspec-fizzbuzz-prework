03aae0b457a07f00bfd00459711ffc032cc2efb5
Brittany Barrow <bangelabarrow@gmail.com>
2018-04-14 17:09:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
-
+def fizzbuzz (int)
+  if int % 3 == 0
+    returns "Fizz"
+  elsif int % 5 == 0
+    returns "Buzz"
+  elsif int % 15 == 0
+    returns "FizzBuzz"
+  else
+    returns "nil"
+  end
 end
